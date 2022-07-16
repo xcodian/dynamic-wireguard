42f5f17e44b66bc5cea24f434af312174734d9d4
xxcodianxx <44289933+xxcodianxx@users.noreply.github.com>
2022-07-17 00:24:12 +0930

    fix(crypto): use lower cost paralellized argon2id
    This makes verification a little bit faster (it used to take 1 second
    sometimes!) as the extra time complexity doesn't make much sense here.

--- src/crypto.rs
+++ src/crypto.rs
@@ -12,9 +12,9 @@ static ARGON2_CONFIG: argon2::Config = argon2::Config {
     variant: argon2::Variant::Argon2id,
     version: argon2::Version::Version13,
     mem_cost: 4096,
-    time_cost: 10,
-    lanes: 1,
-    thread_mode: argon2::ThreadMode::Sequential,
+    time_cost: 1,
+    lanes: 2,
+    thread_mode: argon2::ThreadMode::Parallel,
     secret: &[],
     ad: &[],
     hash_length: 32
